5a7d42d8b502bef272e92c4808e1bc660701584b
leandro.v.059 <leandro.v.059@gmail.com>
2012-01-15 02:51:06 +0000

    Por fin pude hacer que compile la dll con su correspondiente proyecto para pruebas unitarias, ahora puedo empezar en serio (si llego a tener tiempo)

--- trunk/src/app/Colisionador.h
+++ trunk/src/app/Colisionador.h
@@ -1,11 +1,12 @@
-#ifndef COLISIONADOR_H
-#define COLISIONADOR_H
+//#ifndef COLISIONADOR_H
+//#define COLISIONADOR_H
+#pragma once
 
-#include <vector>
+//#include <vector>
 
-namespace Colisionador2D{
+//namespace Colisionador2D{
 
-	struct Circ{
+	/*struct Circ{
 		int x;
 		int y;
 		unsigned int r;
@@ -15,27 +16,28 @@ namespace Colisionador2D{
 
 	struct RectCirc{
 		std::vector<Circ> vectorSup, vectorIzq, vectorInf, vectorDer;
-	};
+	};*/
 
 	//////////////////
 
 	class Rect{
+	
 		int x;
 		int y;
 		unsigned int w;
 		unsigned int h;
 
 	public:
-		Rect(int x, int y, unsigned int w, unsigned int h);
-		RectCirc* Circularizar(unsigned int divisorHorizontal, unsigned int divisorVertical);
+		Rect(int x/*, int y, unsigned int w, unsigned int h*/);
+		//RectCirc* Circularizar(unsigned int divisorHorizontal, unsigned int divisorVertical);
 	};
 
 	//////////////////
 
-    class Colisionador{
+    /*class Colisionador{
 	public:
 		void foo();
-    };
-}
+    };*/
+//}
 
-#endif //COLISIONADOR_H
+//#endif //COLISIONADOR_H
--- trunk/src/test/ColisionadorPruebas.cpp
+++ trunk/src/test/ColisionadorPruebas.cpp
@@ -1,32 +1,44 @@
+// ColisionadorPruebas.cpp: define el punto de entrada de la aplicación de consola.
+//
+
 #include <UnitTest++.h>
-#include "../app/Colisionador.h"
+#include "../app/Colisionador2D.h"
+
+int main(int argc, char* argv[]){
+
+	UnitTest::RunAllTests();
+	system("PAUSE");;
+	return 0;
+}
+
+
 
 using namespace Colisionador2D;
 
 SUITE(PruebasColisionador){
 
-        TEST(RectDeLadosDivisiblesPorElMismoImparSeCircularizaOk){
-                
-                Rect rect(0, 0, 50, 60);
-                RectCirc *rc = rect.Circularizar(5, 5);
+	TEST(RectDeLadosDivisiblesPorElMismoImparSeCircularizaOk){
 
-                //chequeo borde superior e izquierdo, asumo que los otros dos quedan igual
-                unsigned int radio = 2;
-                CHECK_EQUAL(radio, rc->vectorSup[0].x);
-                CHECK_EQUAL(radio, rc->vectorSup[0].y);
-                CHECK_EQUAL(radio, rc->vectorIzq[0].x);
-                CHECK_EQUAL(radio, rc->vectorIzq[0].y);
-                unsigned int contador = 2*radio +1 +2;
-                for(unsigned int i = 1; i < rc->vectorSup.size(); i++){
-                        CHECK_EQUAL(contador, rc->vectorSup[i].x);
-                        CHECK_EQUAL(contador, rc->vectorSup[i].y);
+		Rect rect(4, 0, 50, 60);
+		RectCirc *rc = rect.Circularizar(5, 5);
 
-                        if(rc->vectorIzq.size() > i){
-                                CHECK_EQUAL(contador, rc->vectorIzq[i].x);
-                                CHECK_EQUAL(contador, rc->vectorIzq[i].y);
-                        }
-                        
-                        contador += 2*radio + 1;
-                }
-        }
+		//chequeo borde superior e izquierdo, asumo que los otros dos quedan igual
+		/*unsigned int radio = 2;
+		CHECK_EQUAL(radio, rc->vectorSup[0].x);
+		CHECK_EQUAL(radio, rc->vectorSup[0].y);
+		CHECK_EQUAL(radio, rc->vectorIzq[0].x);
+		CHECK_EQUAL(radio, rc->vectorIzq[0].y);
+		unsigned int contador = 2*radio +1 +2;
+		for(unsigned int i = 1; i < rc->vectorSup.size(); i++){
+			CHECK_EQUAL(contador, rc->vectorSup[i].x);
+			CHECK_EQUAL(contador, rc->vectorSup[i].y);
+
+			if(rc->vectorIzq.size() > i){
+				CHECK_EQUAL(contador, rc->vectorIzq[i].x);
+				CHECK_EQUAL(contador, rc->vectorIzq[i].y);
+			}
+
+			contador += 2*radio + 1;
+		}*/
+	}
 }
